e34b2713c5d3145887dbe0e778bd90fce81193df
aditya <adityabiswas1999@hotmail.com>
2021-09-12 03:53:07 -0700

    holy damn it worked

--- backend/controllers/paymentIntentsController.js
+++ backend/controllers/paymentIntentsController.js
@@ -1,16 +1,19 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.SECRET_KEY);
+const stripe = new Stripe(
+  "sk_test_51JYnmdLGF4kichPF2cEiHEXVu1y7LeWfuAA1f8XW2vuNhFAQ7zJ1ECGnIPn9aywRsrfQzU5f0ZEQok32WELpNdgl00h6YnFCkh"
+);
 
 const paymentIntents = async (req, res) => {
   if (req.method === "POST") {
     try {
       const { amount } = req.body;
-
-      const paymentIntent = await stripe.paymentIntent.create({
+      console.log(amount);
+      const paymentIntent = await stripe.paymentIntents.create({
         amount,
-        currency: "cad",
+        currency: "usd",
       });
+      console.log(paymentIntent);
       res.status(200).send(paymentIntent.client_secret);
     } catch (error) {
       res.status(500).json({ statusCode: 500, message: error.message });
